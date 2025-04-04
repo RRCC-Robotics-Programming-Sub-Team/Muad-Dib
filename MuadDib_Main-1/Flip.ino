615d64183485a2940346eeb1f77f89135fc1f238
MNJ-MC-29 <145073020+merwin29@users.noreply.github.com>
2025-04-04 16:06:20 -0600

    Create Flip.ino

---
+++ MuadDib_Main-1/Flip.ino
@@ -0,0 +1,13 @@
+bool isFlipped = accelZ < -7.0;
+
+// Get distances
+int leftDistance = readUltrasonic(leftTrigPin, leftEchoPin);
+int rightDistance = readUltrasonic(rightTrigPin, rightEchoPin);
+int frontDistance = readUltrasonic(frontTrigPin, frontEchoPin);
+
+// Swap left and right distances if flipped
+if (isFlipped) {
+  int temp = leftDistance;
+  leftDistance = rightDistance;
+  rightDistance = temp;
+}
